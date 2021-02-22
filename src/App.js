5e8333c62189b9d0a11d382ce45ec66802b0da81
Dani Lucaci <danilucaci@gmail.com>
2021-02-22 17:19:37 +0100

    feat: add solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.scss";
 
@@ -22,32 +22,14 @@ function loadItems() {
   }
 }
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [cartItems, setCartItems] = useState(() => loadItems());
 
-    this.state = {
-      cartItems: [],
-    };
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(cartItems));
+  }, [cartItems]);
 
-    this.handleAddToCart = this.handleAddToCart.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  componentDidMount() {
-    this.setState(() => ({
-      cartItems: loadItems(),
-    }));
-  }
-
-  componentDidUpdate() {
-    localStorage.setItem("products", JSON.stringify(this.state.cartItems));
-  }
-
-  handleAddToCart(id) {
-    const cartItems = this.state.cartItems;
-
+  function handleAddToCart(id) {
     let index = cartItems.findIndex((item) => item.id === id);
 
     if (index !== -1) {
@@ -59,32 +41,26 @@ class App extends Component {
         return item;
       });
 
-      this.setState(() => ({
-        cartItems: items,
-      }));
+      setCartItems(items);
     } else {
-      this.setState((prevState) => ({
-        cartItems: [
-          ...prevState.cartItems,
-          {
-            ...products.find((prod) => prod.id === id),
-            quantity: 1,
-          },
-        ],
-      }));
+      setCartItems([
+        ...cartItems,
+        {
+          ...products.find((prod) => prod.id === id),
+          quantity: 1,
+        },
+      ]);
     }
   }
 
-  handleRemove(id) {
-    const items = this.state.cartItems.filter((item) => item.id !== id);
+  function handleRemove(id) {
+    const items = cartItems.filter((item) => item.id !== id);
 
-    this.setState(() => ({
-      cartItems: items,
-    }));
+    setCartItems(items);
   }
 
-  handleChange(event, id) {
-    const items = this.state.cartItems.map((item) => {
+  function handleChange(event, id) {
+    const items = cartItems.map((item) => {
       if (item.id === id) {
         item.quantity = Number(event.target.value);
       }
@@ -92,44 +68,40 @@ class App extends Component {
       return item;
     });
 
-    this.setState(() => ({ cartItems: items }));
+    setCartItems(items);
   }
 
-  render() {
-    const { cartItems } = this.state;
-
-    return (
-      <main className="container-fluid">
-        <div className="row">
-          <div className="col col-6 col-lg-8 p-4">
-            <section className="row row-cols-1">
-              <div className="col">
-                <h1 className="mb-4">Shop</h1>
+  return (
+    <main className="container-fluid">
+      <div className="row">
+        <div className="col col-6 col-lg-8 p-4">
+          <section className="row row-cols-1">
+            <div className="col">
+              <h1 className="mb-4">Shop</h1>
+            </div>
+            <div className="col">
+              <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+                {products.map((product) => (
+                  <ProductCard
+                    key={product.id}
+                    img={product.img}
+                    title={product.title}
+                    price={product.price}
+                    handleAddToCart={() => handleAddToCart(product.id)}
+                  />
+                ))}
               </div>
-              <div className="col">
-                <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4">
-                  {products.map((product) => (
-                    <ProductCard
-                      key={product.id}
-                      img={product.img}
-                      title={product.title}
-                      price={product.price}
-                      handleAddToCart={() => this.handleAddToCart(product.id)}
-                    />
-                  ))}
-                </div>
-              </div>
-            </section>
-          </div>
-          <Cart
-            cartItems={cartItems}
-            handleRemove={this.handleRemove}
-            handleChange={this.handleChange}
-          />
+            </div>
+          </section>
         </div>
-      </main>
-    );
-  }
+        <Cart
+          cartItems={cartItems}
+          handleRemove={handleRemove}
+          handleChange={handleChange}
+        />
+      </div>
+    </main>
+  );
 }
 
 export default App;
